c4e6f7b3977ac2a1cf709043a2534f8462080cd8
Pawel Kaminski <kaminski.pawel404@gmail.com>
2021-12-06 14:17:47 +0100

    Introduce selectbox to _Layout

--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function Category()
+{
+    console.log("dupa");
+    console.log(document.getElementById("categories").value);
+
+}
