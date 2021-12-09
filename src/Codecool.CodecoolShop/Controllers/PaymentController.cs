59da8b0a18995978a5f644852a4b6edb791cae6f
Pawel Kaminski <kaminski.pawel404@gmail.com>
2021-12-09 15:41:19 +0100

    Update Payment form & Inroduce confirmation page

--- src/Codecool.CodecoolShop/Controllers/PaymentController.cs
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -28,6 +28,11 @@ namespace Codecool.CodecoolShop.Controllers
             return View();
         }
 
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
